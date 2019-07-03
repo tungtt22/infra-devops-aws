6eca69801555c8f66b67afd203ab00287d050b62
Tung Tran Thanh <tungtt22@fsoft.com.vn>
2019-07-03 17:32:41 +0700

    Update Security Group for all server

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,13 +1,19 @@
 # Define the security group for public subnet
 resource "aws_security_group" "sgbastion_server" {
-  name        = "bastion"
+  name        = "DevOps-Bastion-SG"
   description = "Allow SSH access on Bastion Server"
+  vpc_id = "${aws_vpc.devops.id}"
+
+  tags = {
+    Name = "DevOps-Bastion-Server-SG"
+  }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH access on Bastion Server"
   }
 
   ingress {
@@ -15,19 +21,114 @@ resource "aws_security_group" "sgbastion_server" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH access on Bastion Server"
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
+  ingress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}"
+    ]
+    description = "Allow SSH resonse from Another Server in VPC to Bastion server"
+  }
 
-  tags = {
-    Name = "Bastion Server SG"
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}"
+    ]
+    description = "Allow SSH access from Bastion Server to another server in VPC"
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH resonse from Bastion Server to another server"
   }
 }
 
 # Define the security group for NAT Instance
 resource "aws_security_group" "sgnat" {
-  name        = "DevOps_NAT_SG"
+  name        = "DevOps-NAT-SG"
   description = "Allow incoming HTTP/HTTPS connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
+
+  tags = {
+    Name = "DevOps-NAT-Instance-SG"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}"
+    ]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}"
+    ]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from NAT Server to another server"
+  }
+}
+
+# Define the security group for NginX
+resource "aws_security_group" "sgnginx" {
+  name        = "DevOps-NginX-SG"
+  description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
+
+  tags = {
+    Name = "DevOps-NginX-Server-SG"
+  }
 
   ingress {
     from_port   = 80
@@ -50,210 +151,332 @@ resource "aws_security_group" "sgnat" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}"
+    ]
+    description = "Allow resonse from another server to NginX Server"
+  }
+
+  egress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}"
+    ]
+    description = "Allow SSH resonse from Bastion Server to another server"
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow resonse from NginX Server to another server"
+  }
+}
+
+# Define the security group for Squid
+resource "aws_security_group" "sgsquid" {
+  name        = "DevOps_Squid_SG"
+  description = "Allow incoming HTTP connections & SSH access"
   vpc_id = "${aws_vpc.devops.id}"
 
   tags = {
-    Name = "NAT Instance SG"
+    Name = "DevOps-Squid-Server-SG"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}"
+    ]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}"
+    ]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+  }
+
+  ingress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow resonse from another server to NginX Server"
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH resonse from Bastion Server to another server"
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH resonse from Bastion Server to another server"
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}",
+      "${var.public_subnet_cidr}",
+      "${var.agent1_subnet_cidr}",
+      "${var.agent2_subnet_cidr}",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse from Server to another server"
   }
 }
 
 # Define the security group for Nexus
 resource "aws_security_group" "sgnexus" {
-  name        = "DevOps_Nexus_SG"
+  name        = "DevOps-Nexus-SG"
   description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
 
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "DevOps-Nexus-Server-SG"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.nginx.private_ip}/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
-
-  tags = {
-    Name = "Nexus Server SG"
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${aws_instance.nginx.private_ip}/32",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 }
 
 # Define the security group for Sonar
 resource "aws_security_group" "sgsonar" {
-  name        = "DevOps_Sonar_SG"
+  name        = "DevOps-Sonar-SG"
   description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
 
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "DevOps-Sonar-Server-SG"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.nginx.private_ip}/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${aws_instance.nginx.private_ip}/32",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
+  }
 
-  tags = {
-    Name = "Sonar Server SG"
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.private1_subnet_cidr}",
+      "${var.private2_subnet_cidr}"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 }
 
 # Define the security group for Jenkins
 resource "aws_security_group" "sgjenkins" {
-  name        = "DevOps_Jenkins_SG"
+  name        = "DevOps-Jenkins-SG"
   description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
 
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "DevOps-Jenkins-Server-SG"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.nginx.private_ip}/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
-
-  tags = {
-    Name = "Jenkins Server SG"
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${aws_instance.nginx.private_ip}/32",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 }
 
 # Define the security group for GitLab
 resource "aws_security_group" "sggitlab" {
-  name        = "DevOps_GitLab_SG"
+  name        = "DevOps-GitLab-SG"
   description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
 
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "DevOps-GitLab-Server-SG"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.nginx.private_ip}/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${aws_instance.nginx.private_ip}/32",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
+  }
 
-  tags = {
-    Name = "GitLab Server SG"
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.private1_subnet_cidr}",
+      "${var.private2_subnet_cidr}"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 }
 
 # Define the security group for Jira
 resource "aws_security_group" "sgjira" {
-  name        = "DevOps_Jira_SG"
+  name        = "DevOps-Jira-SG"
   description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
 
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "DevOps-Jira-Server-SG"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.nginx.private_ip}/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${aws_instance.nginx.private_ip}/32",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
+  }
 
-  tags = {
-    Name = "Jira Server SG"
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${var.private1_subnet_cidr}",
+      "${var.private2_subnet_cidr}"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 }
 
@@ -261,79 +484,46 @@ resource "aws_security_group" "sgjira" {
 resource "aws_security_group" "sgconfluence" {
   name        = "DevOps_Confluence_SG"
   description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
 
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "DevOps-Confluence-Server-SG"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.nginx.private_ip}/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
-
-  tags = {
-    Name = "Confluence Server SG"
-  }
-}
-
-# Define the security group for NginX
-resource "aws_security_group" "sgnginx" {
-  name        = "DevOps_NginX_SG"
-  description = "Allow incoming HTTP connections & SSH access"
-
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
+  egress {
+    from_port   = 32768
+    to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "${aws_instance.nginx.private_ip}/32",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
+  egress {
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  vpc_id = "${aws_vpc.devops.id}"
-
-  tags = {
-    Name = "NginX Server SG"
+    cidr_blocks = [
+      "${var.private1_subnet_cidr}",
+      "${var.private2_subnet_cidr}"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 }
 
@@ -341,71 +531,64 @@ resource "aws_security_group" "sgnginx" {
 resource "aws_security_group" "sgopenldap" {
   name        = "DevOps_OpenLDAP_SG"
   description = "Allow incoming HTTP connections & SSH access"
+  vpc_id = "${aws_vpc.devops.id}"
 
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "DevOps-OpenLDAP Server-SG"
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.nginx.private_ip}/32"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.bastion-server.private_ip}/32"]
   }
 
-  vpc_id = "${aws_vpc.devops.id}"
-
-  tags = {
-    Name = "OpenLDAP Server SG"
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${aws_instance.nginx.private_ip}/32",
+      "${aws_instance.bastion-server.private_ip}/32"
+    ]
+    description = "Allow resonse port from Nexus Server to another server"
   }
 }
 
 # Define the security group for private subnet
 resource "aws_security_group" "sgdb" {
-  name        = "sg_test_web"
-  description = "Allow traffic from public subnet"
+  name        = "DevOps-DB-SG"
+  description = "Allow traffic from appplication subnet"
+  vpc_id = "${aws_vpc.devops.id}"
 
+  tags = {
+    Name = "DevOps-DB-SG"
+  }
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["${var.public_subnet_cidr}"]
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}"
+    ]
   }
 
   ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["${var.public_subnet_cidr}"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["${var.public_subnet_cidr}"]
-  }
-
-  vpc_id = "${aws_vpc.devops.id}"
-
-  tags = {
-    Name = "DB SG"
+    cidr_blocks = [
+      "${var.application1_subnet_cidr}",
+      "${var.application2_subnet_cidr}"
+    ]
   }
 }
